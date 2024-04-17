4579605531f2345938582ffc57c03a232f26b55c
Huzaifa Naeem <159990985+huzaifanaeem1@users.noreply.github.com>
2024-04-17 18:42:22 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,22 @@
+#! /usr/bin/env node
+
+
+// Import the inquirer 
+import inquirer from "inquirer";
+
+// Declare a constant answer
+const answer = await inquirer.prompt([
+    {
+        name: "Sentence",
+        type: "input",
+        message: "Enter your sentence to count the word: "
+    }
+]);
+const words = answer.Sentence.trim().split(" ");
+
+//Print the array[]
+console.log(words)
+
+// print the word count of the sentence to the console
+
+console.log(`Your sentence word count is ${words.length}`)
